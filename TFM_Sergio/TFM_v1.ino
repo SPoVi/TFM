9a5da424ec95427d47c82a1b1cba6807f4126d9d
snopv <snop.vlc@gmail.com>
2021-07-21 19:11:15 +0200

    UPDATE - Refactor TFM_Sergio.ino to TFM_v1.ino

--- TFM_Sergio/TFM_v1.ino
+++ TFM_Sergio/TFM_v1.ino
@@ -1,21 +1,22 @@
-/ Librerias para garantizar el funcionamiento del Wifi y el
+// Librerias para garantizar el funcionamiento del Wifi y el
 // funcionamiento del servidor MQTT
 #include <WiFiNINA.h>
 #include <PubSubClient.h>
 
 WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
+PubSubClient mqttClient(wifiClient); // Instancia de Cliente parcialmente inicializado
 
 // Archivo contenedor de los datos de la red wifi
 #include "arduino_secrets.h"
 
 //Definición de la red Wifi
 
-
 const char* ssid = SECRET_SSID;                   // Nombre de la red WiFi
 const char* password = SECRET_PASS;               // Contraseña de la red WiFi
-const char* mqttServer = RPI_1;    // Dirección ip del servidor ej: test.mosquitto.org
+const char* mqttServer = "test.mosquitto.org";    // Dirección ip del servidor
+//const char* mqttServer = "192.168.85.128";
 
+// Definir topics
 #define topic1 "t1"
 #define topic2 "t2"
 
@@ -24,75 +25,78 @@ int sensorGSR;
 
 long velocidad_deseada = 115200; // baudios
 int port = 1883;
-int pinGSR = A0; // Pin de conexion GSR (A0) D14
+int pinGSR = A0;                 // Pin de conexion GSR (A0) D14
 
-#define mysize 50 // tamaño del vector de datos (paquete)
+#define mysize 10                // tamaño del vector de datos (paquete)
 byte data[mysize];
 
 // Medir tiempos
 unsigned long tStart;
 unsigned long tEnd;
 
+// Paquete datos MQTT
+String paquete = "";
+unsigned long tpaquete;
 
-// Establecemos la conexión Wifi
-void connectToWiFi(){
-  // Connect to WiFi Network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to Wifi");
-  Serial.println("...");
-  Serial.println(ssid);
+// Tiempo real
+// time_t Tstart = now();
 
-  WiFi.begin(ssid, password);
-  int retries = 0;
+// Procedure - Establecemos la conexión Wifi
+void setup_wifi()
+{
+   delay(10);
 
-  while ((WiFi.status() != WL_CONNECTED) && (retries < 15)){
-    retries++;
-    delay(500);
-    Serial.print(".");
-  }
+   Serial.println();
+   Serial.print("Connecting to wifi ");
+   Serial.println(ssid);
 
-  if (retries > 14){
-    Serial.println(F("WiFi connection FAILED"));
-  }
+   WiFi.begin(ssid, password);
 
-  if (WiFi.status() == WL_CONNECTED){
-    Serial.println("WiFi connected!");
-    Serial.println("Local IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-  Serial.println("Setup ready");
+   while (WiFi.status() != WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+
+   randomSeed(micros());   // Semilla aleatoria para micros()
+
+   Serial.println("");
+   Serial.print("WiFi connected: ");
+   Serial.println(ssid);
+   Serial.print("IP address: ");
+   Serial.println(WiFi.localIP());
 }
 
-// Mensaje de SUBSCRUPCION AL TOPIC
-void callback(char* topic, byte* payload, unsigned int len) {
-   Serial.print("Message arrived (topic)[");
-   Serial.print(topic);
-   Serial.print("] ");
-   for (int i = 0; i < len; i++) {
-     Serial.print((char)payload[i]);  // convierte a character el mensaje pasado como int
-   }
-   Serial.println();
+// Funcion String - Crear nuevo cliente
+String NewClientId(){
+     String clientId = "identificacóndelcliente";
+     clientId += String(random(0xffff), HEX);
+     return clientId;
 }
 
 // En caso de perder la conexion reconectar
 void reconnect() {
 // Bucle hasta que nos conectamos
    while (!mqttClient.connected()) {
-     Serial.print("Attempting MQTT connection...");
+     Serial.println("Attempting MQTT connection...");
 
-     // Create a random client ID
-     String clientId = "identificacóndelcliente";
-     clientId += String(random(0xffff), HEX);
+     // New random client ID
+     String clientId = NewClientId();
+     Serial.print("Client id : ");
+     Serial.println(clientId);
 
 
-     // Intento de conexión
+     // Intento de conexión del clienteId creado aleatoriamente
      if (mqttClient.connect(clientId.c_str())) { // Convierte el contenido de una cadena en una cadena terminada en cero al estilo de C
        Serial.println("conectado");
-       // Una vez conectado envíamos un mensaje de que nos hemos conectado a los 3 temas en cuestion
-       mqttClient.publish("tema", "CONECTADO:");
-       // ... and resubscribe
-       mqttClient.subscribe("t1");//Tema al que nos vamos a subscribir para enviar la información
+
+       // Una vez conectado envíamos un mensaje de que nos hemos conectado a los temas en cuestion
+       // bolean publish (topic, payload, [length],[reatined]
+       mqttClient.publish("t1", "CONECTADO:");
+
+       //Tema al que nos vamos a subscribir para enviar la información
+       mqttClient.subscribe("t1");
+
      } else {
        Serial.print("failed, rc=");
        Serial.print(mqttClient.state());
@@ -104,7 +108,7 @@ void reconnect() {
 }
 
 
-// Procedure - Publicar mensaje
+// Procedure - PUBBLICACION al topic
 void pubMQTT(String topic,float topic_val){
   Serial.print("Newest topic " + topic + " value:");
   Serial.println(String(topic_val).c_str());
@@ -117,32 +121,46 @@ void pubMQTT(String topic,float topic_val){
   mqttClient.publish(topic.c_str(), String(topic_val).c_str(), true);
  }
 
+// Procedure - Callback : respuesta a la SUBSCRUPCION de un topic
+void callback(char* topic, byte* payload, unsigned int len) {
+   Serial.print("Message arrived (topic)[");
+   Serial.print(topic);
+   Serial.print("] ");
+   for (int i = 0; i < len; i++) {
+     Serial.print((char)payload[i]);  // convierte a character el mensaje pasado como int
+   }
+   Serial.println();
+}
 
-
+//  ************************************ MAIN SETUP *****************************************
 void setup() {
 
    //Velocidad de los baudios del puerto serial
    Serial.begin(velocidad_deseada); //baudios
 
    //Inicializamos la conexion wifi y la conexion con el servidor
-   connectToWiFi();
+   setup_wifi();
 
+   /*
+    * Los metodos de mqttClient es como su hubieramos puesto al principio
+    * PubSubClient client(server, 1883, callback, ethClient);
+    * PubSubClient client(mqttServer, port, callback, wifiClient);
+    */
    mqttClient.setServer(mqttServer, port);
-   mqttClient.setCallback(callback);  // ????? Subscripcion al topic
+   mqttClient.setCallback(callback);
+   // El cliente está ahora listo para su uso
+
+
 }
 
-String paquete = "";
-unsigned long tpaquete;
 
+// ********************************** MAIN LOOP ********************************************
 void loop() {
 
-   // Comprobar la conexion al servidor.
-   // En el caso de no estar conectado ir a la funcion reconnect y hasta que no se conecta no volver
-   // al codigo principal
+   // Probar conexion a servidor. Si se produce un error en la conexion llamar a reconnect
    if (!mqttClient.connected()){
      reconnect();
    }
-   mqttClient.loop();
 
    //Leemos los valores del sensor GSR
    tStart=micros();             // Inicio del tiempo para coger muestras. OJO MICROSEGUNDOS 10^-6
@@ -172,8 +190,14 @@ void loop() {
     //paquete.concat(" ");
   }
 
-  pubMQTTString(topic1,paquete);
-  pubMQTT(topic2,tEnd);
+  //Publish Values to MQTT broker
+  tpaquete = millis();
+  pubMQTTString(topic1,paquete); // Prublicacion String
+
+
+  pubMQTT(topic2,tEnd);          // Publicacion long
 
   paquete = ""; // Vacía el paquete
+
+  mqttClient.loop();
 }
